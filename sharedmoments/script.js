29732e06468542403b47371e7f39a1099da7f808
hollytriebe <43049090+hollytriebe@users.noreply.github.com>
2018-12-03 00:15:16 -0500

    Add files via upload

---
+++ sharedmoments/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	console.log("ready");
+	  $(".pic1").mouseenter(function() {
+	    $(".pic1").addClass("bright");
+	  });
+	  $(".pic2").mouseenter(function() {
+	    $(".pic2").addClass("bright");
+	  });
+	  $(".pic3").mouseenter(function() {
+	    $(".pic3").addClass("bright");
+	  });
+	  $(".pic4").mouseenter(function() {
+	    $(".pic4").addClass("bright");
+	  });
+	  $(".pic5").mouseenter(function() {
+	    $(".pic5").addClass("bright");
+	  });
+	  $(".pic6").mouseenter(function() {
+	    $(".pic6").addClass("bright");
+	  });
+	  $(".pic7").mouseenter(function() {
+	    $(".pic7").addClass("bright");
+	  });
+	  $(".pic8").mouseenter(function() {
+	    $(".pic8").addClass("bright");
+	  });
+	  $(".pic9").mouseenter(function() {
+	    $(".pic9").addClass("bright");
+	  });
+	  $(".pic10").mouseenter(function() {
+	    $(".pic10").addClass("bright");
+	  });
+});
