dc8882e4c2ea9defe020ba7964f2786fb1432da0
hollytriebe <43049090+hollytriebe@users.noreply.github.com>
2019-02-26 13:54:51 -0500

    Add files via upload

--- clock/script.js
+++ clock/script.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
 	console.log("ready");
-	  $(".pic1").mouseenter(function() {
-	    $(".pic1").addClass("bright");
+	 $(".topred").hover(function() {
+    	$(".topred").toggleClass("reddown"); 
 	  });
+
+	 $(".topred").click(function(){
+	 	$(".numbercircle").addClass("active");
+	 	$(".number").addClass("active");
+	 	$(".smile").addClass("active");
+	 	$(".flashboxleft").addClass("active");
+	 	$(".flashboxright").addClass("active");
+	 	$(".strip").addClass("active");
+	 	  setTimeout(function(){
+	 		$(".numbercircle").removeClass("active");
+	 		$(".number").removeClass("active");
+	 		$(".smile").removeClass("active");
+	 		$(".flashboxleft").removeClass("active");
+	 		$(".flashboxright").removeClass("active");
+  		}, 15000);
+	 })
 });
