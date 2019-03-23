e16b3bdbd26d02c6072c2cfc3273ceb107d8d6a3
hollytriebe <43049090+hollytriebe@users.noreply.github.com>
2019-03-23 15:32:13 -0400

    Add files via upload

--- spriteanimation/script.js
+++ spriteanimation/script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({makeResponsive:true, autoPlay:false, monsterSpeed:300});
+  mugen.initialize({makeResponsive:true, autoPlay:false, monsterSpeed:300, timeBetweenMonsters:3700, attackRadius: 500, monsterHitRadius:500, jumpHeight:250, attackSpeed:600});
 });
