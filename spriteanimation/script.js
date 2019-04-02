0a8695af5b8c06bc663fb9d943e07f1c7c81d1fc
hollytriebe <43049090+hollytriebe@users.noreply.github.com>
2019-04-01 20:48:34 -0400

    Add files via upload

--- spriteanimation/script.js
+++ spriteanimation/script.js
@@ -1,4 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({titleScreenImageURL:"./img/title page horozontal-2.png", makeResponsive:true, heroSpeed: 200, autoPlay:false, monsterSpeed:300, timeBetweenMonsters:3900, attackRadius:50, monsterHitRadius:50, jumpHeight:250, attackDuration:500, pointsPerMonsterKilled:1, monsterMoveToX:150, maxSimultaneousMonsters: 1});
+  mugen.initialize({titleScreenImageURL:"./icons/titlepagehorozontal-2.png", 
+  	makeResponsive:true, 
+  	heroSpeed: 200, 
+  	autoPlay:false, 
+  	monsterSpeed:300, 
+  	timeBetweenMonsters:3900, 
+  	attackRadius:50, 
+  	monsterHitRadius:50, 
+  	jumpHeight:250, 
+  	attackDuration:500, 
+  	pointsPerMonsterKilled:1,
+  	monsterMoveToX:105,
+  	maxSimultaneousMonsters: 1});
 });
